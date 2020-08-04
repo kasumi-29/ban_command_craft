811c683ec042bd86f511d208087df5d6f17b35d4
BuildTools <unconfigured@null.spigotmc.org>
2020-08-04 18:03:56 +0900

    基礎的な枠組みを作成

---
+++ src/main/java/kun/ban_command_craft/ban_command/Main.java
@@ -0,0 +1,9 @@
+package kun.ban_command_craft.ban_command;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Success - (ban_command)");
+    }
+}
